5f991e9f603544cd93e52f43c498cfdcfdd7fa4d
sebastian <skapden@gmail.com>
2024-05-02 08:16:25 -0500

    alright, probably about time to commit these changes

--- html_gen/lib/generators/generate_body.ml
+++ html_gen/lib/generators/generate_body.ml
@@ -1,16 +1,18 @@
 open Generate_image_links
 
-let generate_row row = "<h2>" ^ row ^ "</h><div class=\"scrollgallery\">" ^ generate_image_links row ^ "</div>"
+let generate_row row = "<h2>" ^ Filename.basename row ^ "</h><div class=\"scrollgallery\">" ^ generate_image_links row ^ "</div>"
 
 let generate_rows rows = 
   List.map generate_row rows |> String.concat "\n"
 
-let clean_row_names rows = 
-  List.map Filename.basename rows
+let map_clean_and_unclean_names unclean_names = List.map (fun unclean_name -> (Filename.basename unclean_name, unclean_name)) unclean_names
+
+let sort_mapped_names_by_clean_names mapped_names = List.sort (fun (clean_row_name_a, _) (clean_row_name_b, _) -> String.compare clean_row_name_a clean_row_name_b) mapped_names
 
 let sort_row_names rows = 
   List.rev (List.sort String.compare rows)
 
 open File_interactors.List_directories
 
-let generate_body inner = "<body>" ^ generate_rows ( sort_row_names(clean_row_names(list_directories inner))) ^ "</body>"
+let generate_body inner = "<body>" ^ generate_rows ((list_directories inner)) ^ "</body>"
+
--- html_gen/lib/generators/generate_image_links.ml
+++ html_gen/lib/generators/generate_image_links.ml
@@ -1,2 +1,17 @@
-let generate_image_links image_dir = 
-  List.map File_interactors.List_images.list_images image_dir |> String.concat
+let is_image_file file =
+  let extensions = [".png"; ".jpg"; ".jpeg"; ".gif"; ".bmp"] in
+  List.exists (fun ext -> Filename.check_suffix file ext) extensions
+
+let wrap_with_tag path =
+  Printf.sprintf "<img src=\"%s\">" path
+
+let generate_image_links given_dir =
+  (* Read directory contents *)
+  let files = Array.to_list (Sys.readdir given_dir) in
+
+  (* Filter and generate img tags *)
+  let images = List.filter is_image_file files in
+  let image_tags = List.map (fun img -> wrap_with_tag (Filename.concat given_dir img)) images in
+
+  (* Concatenate all image tags into a single string *)
+  String.concat "\n" image_tags
