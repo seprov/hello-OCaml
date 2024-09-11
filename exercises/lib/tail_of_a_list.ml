34bcc8d22e8818f1d55f6193b4ef4aa070e86bbe
seprov <provenzano.sebastian@gmail.com>
2024-09-11 10:35:19 -0500

    use rec

--- exercises/lib/tail_of_a_list.ml
+++ exercises/lib/tail_of_a_list.ml
@@ -1,13 +1,7 @@
-let last foo =
-  match List.is_empty foo with
-  | true -> None
-  | _ -> Some (List.nth foo (List.length foo - 1))
+let rec last = function [] -> None | [ x ] -> Some x | _ :: t -> last t
 
-let last_two foo =
-  match List.length foo with
-  | x when x >= 2 ->
-      Some
-        [
-          List.nth foo (List.length foo - 2); List.nth foo (List.length foo - 1);
-        ]
-  | _ -> None
+let rec last_two = function
+  | [] | [ _ ] -> None
+  | [ x; y ] -> Some [ x; y ]
+  | _ :: t -> last_two t
+
